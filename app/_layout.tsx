65f98eb8cdf2a8f1f86b381e5f38fe51eb87866d
HanVbear <ryoitenkaii@gmail.com>
2025-12-23 17:45:09 +0700

    set up font and colors and add some assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./global.css";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
+    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+    "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+    "BeauRivage": require('../assets/fonts/BeauRivage.ttf'),
+    "Kalnia": require('../assets/fonts/Kalnia.ttf'),
+  })
+
+  useEffect(() => {
+    if(fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if(!fontsLoaded) return null;
+
   return <Stack />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
   content: ["./App.tsx", "./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "rubik": ['Rubik Regular', 'sans-serif'],
+        "rubik-bold": ['Rubik-Bold', 'sans-serif'],
+        "rubik-semibold": ['Rubik-SemiBold', 'sans-serif'],
+        "rubik-medium": ['Rubik-Medium', 'sans-serif'],
+        "beaurivage": ['BeauRivage', 'sans-serif'],
+        "kalnia": ['Kalnia', 'sans-serif'],
+      },
+      colors: {
+        "navy": "2f4156",
+        "teal": "567c8d",
+        "skyblue": "c8d9e6",
+        "beige": "f5efeb",
+        "white": "ffffff",
+        "cream": "b39a84",
+      }
+    },
   },
   plugins: [],
 }
