6c5b8e5b606f4b5d523dd7b700e7ed9abf0908d8
HanVbear <ryoitenkaii@gmail.com>
2025-12-23 10:13:25 +0700

    add some starting code and set up router

---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,15 @@
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+
+const Property = () => {
+    const { id } = useLocalSearchParams();
+
+    return (
+        <View>
+            <Text>Property {id}</Text>
+        </View>
+    )
+}
+
+export default Property;
