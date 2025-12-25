739d0c6ab4f0886c68183ee73636187cb91fbe1a
HanVbear <ryoitenkaii@gmail.com>
2025-12-25 14:12:23 +0700

    add search feature

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,21 +1,31 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-
-      <Text className="text-3xl my-10 text-navy font-bbh-bogle text-center">Welcome to Hansbilding</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+              <Text className="text-base font-rubik-medium text-black-300">oblivion</Text>
+              <Image source={icons.bell} className="size-6" />
+            </View>
+          </View>
+        </View>
+        <Search />
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-bbh-bartle text-teal">Featured</Text>
+          <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-black-300">See Al</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ components/Search.tsx
@@ -0,0 +1,38 @@
+import icons from "@/constants/icons";
+import { router, useLocalSearchParams, usePathname } from "expo-router";
+import { useState } from "react";
+import { Image, TextInput, TouchableOpacity, View } from "react-native";
+import { useDebouncedCallback } from "use-debounce";
+
+const Search = () => {
+    const path = usePathname();
+    const params = useLocalSearchParams<{ query?: string }>();
+    const [search, setSearch] = useState(params.query);
+
+    const debounchedSearch = useDebouncedCallback((text: string ) => router.setParams({ query: text }), 500)
+
+    const handleSearch = (text: string) => {
+        setSearch(text);
+        debounchedSearch(text);
+    }
+
+    return (
+        <View className="flex flex-row items-center justify-between w-full rounded-lg bg-beige border border-teal mt-5 py-2">
+            <View className="flex-1 flex flex-row items-center justify-start z-50">
+                <Image source={icons.search} className="size-5" />
+                <TextInput 
+                    value={search}
+                    onChangeText={handleSearch}
+                    placeholder="Search for your ideal house here..."
+                    className="text-sm font-rubik text-black-300 ml-2 flex-1"
+                />
+            </View>
+
+            <TouchableOpacity>
+                <Image source={icons.filter} className="size-5" />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default Search;
